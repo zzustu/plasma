b9fd3acc118a7b1c661c8e366034da5c752b14a7
Tingyan Shen <tingyan.shen@gmail.com>
2020-11-07 11:48:10 +0800

    Update @deepinthink-plasma plasma-broker dependencies (#4)

--- jcloud-routing-server/src/main/java/org/deepinthink/jcloud/routing/server/RoutingServerApplication.java
+++ jcloud-routing-server/src/main/java/org/deepinthink/jcloud/routing/server/RoutingServerApplication.java
@@ -16,9 +16,11 @@
 
 package org.deepinthink.jcloud.routing.server;
 
+import org.deepinthink.plasma.broker.client.EnableBrokerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBrokerClient
 @SpringBootApplication
 public class RoutingServerApplication {
   public static void main(String[] args) {
